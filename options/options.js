5aa5a2a5c3b80a2dbeecfecdc817a6e7c3fbd568
daudierd <daudierd@users.noreply.github.com>
2018-08-17 18:50:32 +0200

    Add YouTube Data API options
    - Add checkbox & fields for enabling YouTube DataAPI
    - Store options in local storage

--- options/options.js
+++ options/options.js
@@ -2,6 +2,9 @@ const searchMode = {LOAD: 'load', QUEUE: 'queue'}
 
 const loadSearchOption = document.getElementById('load-search');
 const queueSearchOption = document.getElementById('queue-search');
+const dataApiCheckbox = document.getElementById('data-api-checkbox');
+const dataApiClientID = document.getElementById('data-api-clientid');
+const dataApiKey = document.getElementById('data-api-key');
 
 const updateOptions = function(payload) {
   browser.storage.local.get('options').then((res) => {
@@ -14,6 +17,16 @@ const updateOptions = function(payload) {
   })
 }
 
+const displayAPIfields = function(cond) {
+  if(cond) {
+    dataApiClientID.style.removeProperty('display');
+    dataApiKey.style.removeProperty('display');
+  } else {
+    dataApiClientID.style.display = 'none';
+    dataApiKey.style.display = 'none';
+  }
+}
+
 initialize();
 function initialize() {
   browser.storage.local.get('options').then((res) => {
@@ -21,6 +34,12 @@ function initialize() {
       loadSearchOption.checked = true;
     } else {
       queueSearchOption.checked = true;
+    }
+    displayAPIfields(res.options.enableDataAPI);
+    dataApiCheckbox.checked = res.options.enableDataAPI;
+    if(res.options.enableDataAPI) {
+      dataApiClientID.value = res.options.dataApiClientID;
+      dataApiKey.value = res.options.dataApiKey;
     }
   })
 }
@@ -32,3 +51,24 @@ loadSearchOption.onclick =  function() {
 queueSearchOption.onclick =  function() {
   updateOptions({searchMode: searchMode.QUEUE})
 }
+
+dataApiCheckbox.onclick = function(e) {
+  updateOptions({enableDataAPI: e.target.checked})
+}
+
+dataApiClientID.onkeydown = function(e) {
+  updateOptions({dataApiClientID: e.target.value})
+}
+
+dataApiKey.onkeydown = function(e) {
+  updateOptions({dataApiKey: e.target.value})
+}
+
+browser.storage.onChanged.addListener(function(changes, areaName) {
+  if(areaName == 'local') {
+    if(changes.options){
+      displayAPIfields(changes.options.newValue.enableDataAPI);
+      console.log('Options changed:', changes.options.newValue);
+    }
+  }
+})
